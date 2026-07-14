a7c48bc648de38cfe6cc8c88bd913dac692f306f
Saravanesh-Git <saravanesh962006@gmail.com>
2026-07-14 18:39:43 +0000

    Wrote the main function in main.py

--- supplychain-guard/main.py
+++ supplychain-guard/main.py
@@ -4,3 +4,28 @@
 # - Call scanner
 # - Show report
 # - Ask proceed/cancel
+
+import sys, subprocess
+from package_managers.pip_manager import pip_dry_run
+
+def main():
+    args = sys.argv[1:]
+    
+    if not args:
+        print("Usage: supplyguard <package manager> install <package>")
+        return
+    
+    manager = args[0]
+    real_args = args[1:]
+
+    print(f"Scanning command: {manager} {' '.join(real_args)}")
+    command = f"{manager} {' '.join(real_args)}"
+
+    if manager == 'pip':
+        pip_dry_run(command)
+
+    elif manager == 'npm':
+        pass
+
+if __name__ == "__main__":
+    main()
--- supplychain-guard/package_managers/pip_manager.py
+++ supplychain-guard/package_managers/pip_manager.py
@@ -2,3 +2,8 @@
 # - Generate dependency report
 # - Parse package names and versions
 # - Return normalized dependency list
+
+import subprocess
+
+def pip_dry_run(command):
+    pass
